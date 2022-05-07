bc118930fd1d15e4bf5dc8db58a3b6630b28293c
chowkokkeong <50937950+chowkokkeong@users.noreply.github.com>
2022-05-07 13:13:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.clifhgpky1m3.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "cpac3009"
+customdb = "employee"
+custombucket = "chowkokkeong-employee"
+customregion = "us-east-1"
